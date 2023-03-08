28a0ebf349e27694f39828887fb87a1850dff0d6
vladlazarev1998 <vladlazarev1998@gmail.com>
2023-03-08 12:08:45 +0200

    Add lang function to label

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -5,51 +5,51 @@
         <form method="POST" action="{{ route('mailer.store') }}">
             @csrf
             <div class="form-group mb-4">
-                <label for="from">From</label>
+                <label for="from">{{ __('From') }}</label>
                 <input type="email" class="form-control" name="from" id="from" value="{{ old('from') }}">
                 @error('from')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group mb-4">
-                <label for="to">To</label>
+                <label for="to">{{ __('To') }}</label>
                 <input type="email" class="form-control" name="to" id="to" value="{{ old('to') }}">
                 @error('to')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group mb-4">
-                <label for="cc">CC</label>
+                <label for="cc">{{ __('CC') }}</label>
                 <input type="email" class="form-control" name="cc" id="cc" value="{{ old('cc') }}">
                 @error('cc')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group mb-4">
-                <label for="subject">Subject</label>
+                <label for="subject">{{ __('Subject') }}</label>
                 <input type="text" class="form-control" name="subject" id="subject" value="{{ old('subject') }}">
                 @error('subject')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group mb-4">
-                <label for="type">Type</label>
+                <label for="type">{{ __('Type') }}</label>
                 <select class="form-control" name="type" id="type">
-                    <option value="text">Text</option>
-                    <option value="html">HTML</option>
+                    <option value="text">{{ __('Text') }}</option>
+                    <option value="html">{{ __('HTML') }}</option>
                 </select>
                 @error('type')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
             <div class="form-group mb-4">
-                <label for="body">Body</label>
+                <label for="body">{{ __('Body') }}</label>
                 <textarea class="form-control" id="body" name="body" rows="3">{{ old('body') }}</textarea>
                 @error('body')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
-            <button type="submit" class="btn btn-primary">Send</button>
+            <button type="submit" class="btn btn-primary">{{ __('Send') }}</button>
         </form>
     </div>
 @endsection
